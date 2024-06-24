79acda01cfa73302312678970cf7142cdd69bae4
chaelim090908 <cofla090908@gmail.com>
2024-06-25 00:17:57 +0900

    0624 울유 wip

---
+++ 0624_eulyoo/js/main.js
@@ -0,0 +1,15 @@
+const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+	effect: "fade", /* fade 효과 */
+
+	autoplay: {  /* 팝업 자동 실행 */
+		delay: 5000,
+		disableOnInteraction: true,
+	},
+	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+	navigation: {  /* 이전, 다음 버튼 */
+		nextEl: '.visual .bnt_next',  /* 다음 버튼의 클래스명 */
+		prevEl: '.visual .bnt_prev',  
+	},
+
+});
