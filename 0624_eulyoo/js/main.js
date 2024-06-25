501f96897274976d69af3b1536e4ddc96699c4af
chaelim090908 <cofla090908@gmail.com>
2024-06-25 23:57:16 +0900

    0625 을유문화사 visual 코딩

--- 0624_eulyoo/js/main.js
+++ 0624_eulyoo/js/main.js
@@ -1,15 +1,19 @@
-const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+$(document).ready(function(){
+	const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
-	effect: "fade", /* fade 효과 */
+		effect: "fade", /* fade 효과 */
 
-	autoplay: {  /* 팝업 자동 실행 */
-		delay: 5000,
-		disableOnInteraction: true,
-	},
-	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-	navigation: {  /* 이전, 다음 버튼 */
-		nextEl: '.visual .bnt_next',  /* 다음 버튼의 클래스명 */
-		prevEl: '.visual .bnt_prev',  
-	},
+		autoplay: {  /* 팝업 자동 실행 */
+			delay: 2500,
+			disableOnInteraction: true,
+		},
+		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+		navigation: {  /* 이전, 다음 버튼 */
+			nextEl: '.visual .bnt_next',  /* 다음 버튼의 클래스명 */
+			prevEl: '.visual .bnt_prev',  
+		},
 
-});
+	}); //visual_swiper
+})
+
+
