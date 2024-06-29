f0c8aadf456ff394989b81343133115b1150bd66
chaelim090908 <cofla090908@gmail.com>
2024-06-29 14:38:23 +0900

    0629 을유 wip

--- 0624_eulyoo/js/main.js
+++ 0624_eulyoo/js/main.js
@@ -17,17 +17,13 @@ $(document).ready(function(){
 
 	const book_swiper = new Swiper('.book .swiper', { /* 팝업을 감싼는 요소의 class명 */
 		slidesPerView: "auto", /* li의 넓이 비율로 안함 - css에서 준 넓이대로 함 */
-		spaceBetween: 24, /* li와 li사이 - 제일 작은 여백 */
+		spaceBetween: 16, /* li와 li사이 - 제일 작은 여백 */
 		breakpoints: {
 			640: {  /* 640px 이상이 되면 적용 */
-				spaceBetween: 16, 
+				spaceBetween: 24, 
 			},
 		},
 		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-		autoplay: {  /* 팝업 자동 실행 */
-			delay: 5000,
-			disableOnInteraction: true,
-		},
 	});
 })
 
